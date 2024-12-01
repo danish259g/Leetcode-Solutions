50a9ebac4e7883500a66cf98ec63c3c2f2604833
Daniel Shmulevich <259danish@gmail.com>
2024-12-01 22:53:32 +0200

    Added solution for LengthOfLongestSubstring problem

---
+++ LengthOfLongestSubstring.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0)
+            return 0;
+        if (s.length() == 1)
+            return 1;
+
+        int maxLength = 0;
+        int left = 0, right = 1;
+        HashSet<Character> hist = new HashSet<>();
+        hist.add(s.charAt(0));
+
+        while (left < s.length() && right < s.length()) {
+
+            // push left to reach to a legal substring
+            while (hist.contains(s.charAt(right))) {
+                hist.remove(s.charAt(left));
+                left++;
+            }
+
+            // add the right char to the substring since its legal now
+            hist.add(s.charAt(right));
+            right++;
+
+            if (hist.size() > maxLength)
+                maxLength = hist.size();
+        }
+        return maxLength;
+    }
+}
