84c2c1e7b105cbf6091a1c09023584afdc6acf92
Daniel Shmulevich <259danish@gmail.com>
2024-12-10 22:34:46 +0200

    Added LongestConsecutiveSequence

---
+++ LongestConsecutiveSequence.py
@@ -0,0 +1,23 @@
+"""
+Problem - 128 Longest Consecutive Sequence
+Link - https://leetcode.com/problems/longest-consecutive-sequence/description/
+"""
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        longest = 0
+        nums_set = set(nums)
+
+        for num in nums_set:
+            if (num - 1) not in nums_set: # n is the start of some sequence 
+                length = 1
+                while (num + length) in nums_set:
+                    length += 1 
+                longest = max(longest, length)
+        
+        return longest 
